0b4cfbe5a3926ab94be49e9dba231714e8270f46
Hans - Kristjan <kristjanladva@gmail.com>
2019-09-14 08:37:00 +0300

    JSON logic

--- upload.php
+++ upload.php
@@ -4,12 +4,54 @@ function showResult($text, $isError = true)
 {
     return "<p class='" . ($isError ? 'msg-error' : 'msg-success') . "'>" . htmlspecialchars($text) . "</p>";
 }
+
+function validName($string)
+{
+    return !preg_match('/^(\w+)(\s?)(\d+)$/', $string);
+}
+function validPhone($string)
+{
+    return !preg_match('/^(?!-)(?=.[0-9])[- ()0-9]+(?<!-)$/', $string);
+}
+function validAddress($string)
+{
+    return !preg_match('/[^,:{}\\[\\]0-9.\\-+Eaeflnr-u \\n\\r\\t]/', $string);
+}
+function validWebsite($string)
+{
+    return !preg_match('/^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9-\.]){1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})(\/\S*)?)$/', $string);
+}
+
+function isMultiArray($arrays)
+{
+    foreach ($arrays as $array) {
+        if (is_array($array)) return true;
+    }
+    return false;
+}
+
+function checkKeys($array, $obj)
+{
+    if (!isset($array['name'])) {
+        echo showResult('JSON object number: ' . $obj . ' is missing key "name"', true);
+    }
+    if (!isset($array['phone'])) {
+        echo showResult('JSON object number: ' . $obj . ' is missing key "phone"', true);
+    }
+    if (!isset($array['address'])) {
+        echo showResult('JSON object number: ' . $obj . ' is missing key "address"', true);
+    }
+    if (!isset($array['website'])) {
+        echo showResult('JSON object number: ' . $obj . ' is missing key "website"', true);
+    }
+}
+
 //faili loogika
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['file']) && $_FILES['file']['error'] == UPLOAD_ERR_OK) {
         $errors = [];
         $ext = 'json'; //lubatud faililaiend
-        $path = './uploads/'; // WWWst rootist välja
+        $path = './uploads/'; // WWWst rootist välja ?chmod("/uploads/sha1Named", 0644); + .htaccess?
         $file_name = $_FILES['file']['name'];
         $file_size = $_FILES['file']['size'];
         $file_tmp = $_FILES['file']['tmp_name'];
@@ -25,26 +67,91 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
         // mime
         $mime = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $file_tmp);
-        if($mime !== 'application/json') {
+        if ($mime !== 'application/json') {
             $errors[] = 'Invalid file format';
         }
 
         //nime muutmine ja kausta liigutamine
         if (!move_uploaded_file(
             $file_temp,
-            sprintf('./uploads/%s.%s',
+            sprintf(
+                './uploads/%s.%s',
                 sha1_file($file_temp),
                 $ext
             )
-        ))
+        )) {
+            $errors[] = 'Failed to move the uploaded file.';
+        }
     }
-
 }
 
 //json loogika
-$jsondata = file_get_contents($fileNewName);
-$json = json_decode($jsondata, true);
-$requiredKeys = ['nimi', 'telefon', 'aadress', 'veebileht'];
+if (empty($errors)) {
+    $jsondata = file_get_contents($fileNewName);
+    $json = json_decode($jsondata, true);
+    $requiredKeys = ['name', 'phone', 'address', 'website'];
+    if (isMultiArray($json)) {
+        $objectNumber = 0;
+        foreach ($json as $array => $keys) {
+            $objectNumber++;
+            checkKeys($json[$array], $objectNumber);
+            foreach ($json[$array] as $key => $value) {
+                switch ($key) {
+                    case "name":
+                        if (validName($value)) {
+                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "name" is incorrect', true);
+                        }
+                        break;
+                    case "address":
+                        if (validAddress($value)) {
+                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "address" is incorrect', true);
+                        }
+                        break;
+                    case "phone":
+                        if (validPhone($value)) {
+                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "phone" is incorrect', true);
+                        }
+                        break;
+                    case "website":
+                        if (validWebsite($value)) {
+                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "website" is incorrect', true);
+                        }
+                        break;
+                    default:
+                        echo showResult('JSON object number: ' . $objectNumber . ' contains illegal key: ' . $key, true);
+                }
+            }
+        }
+    } else {
+        checkKeys($json, 1);
+        foreach ($json as $key => $value) {
+            switch ($key) {
+                case "name":
+                    if (validName($value)) {
+                        echo showResult('Value of key "name" is incorrect', true);
+                    }
+                    break;
+                case "address":
+                    if (validAddress($value)) {
+                        echo showResult('Value of key "address" is incorrect', true);
+                    }
+                    break;
+                case "phone":
+                    if (validPhone($value)) {
+                        echo showResult('Value of key "phone" is incorrect', true);
+                    }
+                    break;
+                case "website":
+                    if (validWebsite($value)) {
+                        echo showResult('Value of key "website" is incorrect', true);
+                    }
+                    break;
+                default:
+                    echo showResult('JSON object contains illegal key: ' . $key, true);
+            }
+        }
+    }
+}
 
 //veebileht  ^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9-\.]){1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,}))$
 // telo ^(?!-)(?=.[0-9])[- ()0-9]+(?<!-)$
