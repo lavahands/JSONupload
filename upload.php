ab1f4612fa24d049a5a3b7dd84bc9e8ffe3b2efb
Hans - Kristjan <kristjanladva@gmail.com>
2019-09-16 10:29:51 +0300

    Requirements done

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@ function validPhone($string)
 }
 function validAddress($string)
 {
-    return !preg_match('/[^,:{}\\[\\]0-9.\\-+Eaeflnr-u \\n\\r\\t]/', $string);
+    return !preg_match('/^[\w]+[\w\/., ]+$/', $string);
 }
 function validWebsite($string)
 {
@@ -33,16 +33,16 @@ function isMultiArray($arrays)
 function checkKeys($array, $obj)
 {
     if (!isset($array['name'])) {
-        echo showResult('JSON object number: ' . $obj . ' is missing key "name"', true);
+        $jerrors[] = 'JSON object number: ' . $obj . ' is missing key "name"';
     }
     if (!isset($array['phone'])) {
-        echo showResult('JSON object number: ' . $obj . ' is missing key "phone"', true);
+        $jerrors[] = 'JSON object number: ' . $obj . ' is missing key "phone"';
     }
     if (!isset($array['address'])) {
-        echo showResult('JSON object number: ' . $obj . ' is missing key "address"', true);
+        $jerrors[] = 'JSON object number: ' . $obj . ' is missing key "address"';
     }
     if (!isset($array['website'])) {
-        echo showResult('JSON object number: ' . $obj . ' is missing key "website"', true);
+        $jerrors[] = 'JSON object number: ' . $obj . ' is missing key "website"';
     }
 }
 
@@ -62,106 +62,104 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
         }
         // laiendi kontroll
-        if (ext !== strtolower(end($file_ext))) {
+        if ($ext !== strtolower(end($file_ext))) {
             $errors[] = 'Forbidden extention: ' . $file_name . ' ' . $file_type;
         }
         // mime
         $mime = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $file_tmp);
-        if ($mime !== 'application/json') {
-            $errors[] = 'Invalid file format';
+        if ($mime !== 'text/plain') {
+            $errors[] = 'Invalid file format: ' . $mime;
         }
 
-        //nime muutmine ja kausta liigutamine
-        if (!move_uploaded_file(
-            $file_temp,
-            sprintf(
-                './uploads/%s.%s',
-                sha1_file($file_temp),
-                $ext
-            )
-        )) {
-            $errors[] = 'Failed to move the uploaded file.';
-        }
-    }
-}
-
-//json loogika
-if (empty($errors)) {
-    $jsondata = file_get_contents($fileNewName);
-    $json = json_decode($jsondata, true);
-    $requiredKeys = ['name', 'phone', 'address', 'website'];
-    if (isMultiArray($json)) {
-        $objectNumber = 0;
-        foreach ($json as $array => $keys) {
-            $objectNumber++;
-            checkKeys($json[$array], $objectNumber);
-            foreach ($json[$array] as $key => $value) {
-                switch ($key) {
-                    case "name":
-                        if (validName($value)) {
-                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "name" is incorrect', true);
+        if (empty($errors)) {
+            $jerrors = [];
+            $jsondata = file_get_contents($file_tmp);
+            $requiredKeys = ['name', 'phone', 'address', 'website'];
+            $json = json_decode($jsondata, true);
+            if ($json == null || json_last_error() !== JSON_ERROR_NONE) {
+                $jerrors[] = 'Invalid JSON format';
+            } else {
+                if (isMultiArray($json)) {
+                    $objectNumber = 0;
+                    foreach ($json as $array => $keys) {
+                        $objectNumber++;
+                        checkKeys($json[$array], $objectNumber);
+                        foreach ($json[$array] as $key => $value) {
+                            switch ($key) {
+                                case "name":
+                                    if (validName($value)) {
+                                        $jerrors[] = 'JSON object number: ' . $objectNumber . ' value of key "name" is incorrect';
+                                    }
+                                    break;
+                                case "address":
+                                    if (validAddress($value)) {
+                                        $jerrors[] = 'JSON object number: ' . $objectNumber . ' value of key "address" is incorrect';
+                                    }
+                                    break;
+                                case "phone":
+                                    if (validPhone($value)) {
+                                        $jerrors[] = 'JSON object number: ' . $objectNumber . ' value of key "phone" is incorrect';
+                                    }
+                                    break;
+                                case "website":
+                                    if (validWebsite($value)) {
+                                        $jerrors[] = 'JSON object number: ' . $objectNumber . ' value of key "website" is incorrect';
+                                    }
+                                    break;
+                                default:
+                                    $jerrors[] = 'JSON object number: ' . $objectNumber . ' contains illegal key: ' . $key;
+                            }
                         }
-                        break;
-                    case "address":
-                        if (validAddress($value)) {
-                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "address" is incorrect', true);
+                    }
+                } else {
+                    checkKeys($json, 1); //Single object json file
+                    foreach ($json as $key => $value) {
+                        switch ($key) {
+                            case "name":
+                                if (validName($value)) {
+                                    $jerrors[] = 'Value of key "name" is incorrect';
+                                }
+                                break;
+                            case "address":
+                                if (validAddress($value)) {
+                                    $jerrors[] = 'Value of key "address" is incorrect';
+                                }
+                                break;
+                            case "phone":
+                                if (validPhone($value)) {
+                                    $jerrors[] = 'Value of key "phone" is incorrect';
+                                }
+                                break;
+                            case "website":
+                                if (validWebsite($value)) {
+                                    $jerrors[] = 'Value of key "website" is incorrect';
+                                }
+                                break;
+                            default:
+                                $jerrors[] = 'JSON object contains illegal key: ' . $key;
                         }
-                        break;
-                    case "phone":
-                        if (validPhone($value)) {
-                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "phone" is incorrect', true);
-                        }
-                        break;
-                    case "website":
-                        if (validWebsite($value)) {
-                            echo showResult('JSON object number: ' . $objectNumber . ' value of key "website" is incorrect', true);
-                        }
-                        break;
-                    default:
-                        echo showResult('JSON object number: ' . $objectNumber . ' contains illegal key: ' . $key, true);
+                    }
                 }
             }
+            // JSON vigade puudumisel faili ümbernimetamine sha1-e ja liigutamine /uploads kausta
+            if (empty($jerrors)) {
+                if (!move_uploaded_file($file_tmp, sprintf('./uploads/%s.%s', sha1_file($file_tmp), $ext))) {
+                    $errors[] = 'Failed to move the uploaded file.';
+                } else {
+                    echo showResult('File was uploaded successfully', false);
+                }
+            } else {
+                foreach ($jerrors as $jerror) {
+                    echo showResult($jerror, true);
+                }
+                echo showResult('Due to errors JSON file was not uploaded.', true);
+            }
         }
-    } else {
-        checkKeys($json, 1);
-        foreach ($json as $key => $value) {
-            switch ($key) {
-                case "name":
-                    if (validName($value)) {
-                        echo showResult('Value of key "name" is incorrect', true);
-                    }
-                    break;
-                case "address":
-                    if (validAddress($value)) {
-                        echo showResult('Value of key "address" is incorrect', true);
-                    }
-                    break;
-                case "phone":
-                    if (validPhone($value)) {
-                        echo showResult('Value of key "phone" is incorrect', true);
-                    }
-                    break;
-                case "website":
-                    if (validWebsite($value)) {
-                        echo showResult('Value of key "website" is incorrect', true);
-                    }
-                    break;
-                default:
-                    echo showResult('JSON object contains illegal key: ' . $key, true);
+
+        if ($errors) {
+            foreach ($errors as $err) {
+                echo showResult($err, true);
             }
         }
     }
 }
-
-//veebileht  ^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9-\.]){1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,}))$
-// telo ^(?!-)(?=.[0-9])[- ()0-9]+(?<!-)$
-
-// Errorid
-// Kas on json fail ja formaat
-// Faili sisuks olev json sisaldab väljasid: nimi, telefoni number, aadress ja veebileht. Peavad
-// eksisteerima ja olema täidetud.
-// Telefoni number võib sisaldada ainult numbreid (0-9), sidekriipsu (-), tühikut ja sulgusid
-// Veebileht peaks olema süntaktiliselt korrektne, ehk sisaldama “domeen.tld” (näiteks
-// https://www.scr.ee/minuveeb ja scr.ee on mõlemad lubatavad)
-// Faili sees võib olla üks JSON objekt või mittu objekti massiivina.
-// Pärast valideerimist tagastada aruanne iga objekti kohta, kas ja millised puudused esinesid.
